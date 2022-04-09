63f7eff03111d7fe3f8a1fa229ba9f01aa3b3193
RoIs889 <88293319+RoIs889@users.noreply.github.com>
2022-04-09 20:30:09 +0300

    Add files via upload

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -2,13 +2,13 @@ var isImg1=true;
 var isImg2=false;
 function bodyImg(){
 	if(isImg1==true){
-		document.body.style.backgroundImage='url("../assets/light_background.jpg")';
+		document.body.style.backgroundColor='#09101d';
 		document.body.style.color='black';
 		isImg1=false;
 		isImg2=true;
 	}
 	else if(isImg2==true){
-		document.body.style.backgroundImage='url("../assets/dark_background.png")';
+		document.body.style.backgroundColor='#36454F';
 		isImg1=true;
 		isImg2=false;
 	}
